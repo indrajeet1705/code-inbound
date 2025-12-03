3b36e433878bfc32bf61a3735fca889b2ef1d89b
indrajeet bhujabl <indrajeetbhujbal0@gmail.com>
2025-12-03 10:50:08 +0530

    added origin

--- user-management-backend/src/main.ts
+++ user-management-backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{cors:true});
   app.enableCors({
-  origin: ['http://localhost:5173', 'http://localhost:3000','https://code-inbound.onrender.com'],
+  origin: ['http://localhost:5173', 'http://localhost:3000','https://code-inbound.onrender.com','https://code-inbound-git-main-indrajeets-projects-18359aa1.vercel.app'],
   methods: 'GET,POST,PUT,PATCH,DELETE',
   allowedHeaders: 'Content-Type, Authorization',
   credentials: true
