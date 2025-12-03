9446d85111c67f1a24b14411085da61dd9197bc0
indrajeet bhujabl <indrajeetbhujbal0@gmail.com>
2025-12-03 10:36:01 +0530

    database changes

--- user-management-backend/src/app.module.ts
+++ user-management-backend/src/app.module.ts
@@ -11,19 +11,17 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     ConfigModule.forRoot({isGlobal:true}),
     TypeOrmModule.forRootAsync({
-      useFactory: ( configService :ConfigService)=>({
-          type:'postgres',
-          host:configService.get('DB_HOST'),
-          port:+configService.get('DB_PORT'),
-          username:configService.get('DB_USERNAME'),
-          password:configService.get('DB_PASSWORD'),
-          database:configService.get('DB_DATABASE'),
-          entities: [__dirname + '/**/*.entity{.ts,.js}'],
-          synchronize:true
-
-      }),
-      inject:[ConfigService]
-    }),
+    useFactory: (configService: ConfigService) => ({
+    type: 'postgres',
+    url: configService.get<string>('DATABASE_URL'),
+    autoLoadEntities: true,
+    synchronize: true,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  }),
+  inject: [ConfigService],
+}),
     TaskModule,
     UserModule,
     AuthModule
