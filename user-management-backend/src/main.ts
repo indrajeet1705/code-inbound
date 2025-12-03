6e0f0de11464d979cd70567cad3abe955d0c4d6a
indrajeet bhujabl <indrajeetbhujbal0@gmail.com>
2025-12-03 10:43:43 +0530

    CORS update

--- user-management-backend/src/main.ts
+++ user-management-backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{cors:true});
   app.enableCors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:3000','https://code-inbound.onrender.com'],
   methods: 'GET,POST,PUT,PATCH,DELETE',
   allowedHeaders: 'Content-Type, Authorization',
   credentials: true
